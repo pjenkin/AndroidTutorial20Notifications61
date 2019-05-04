ac03a0e0a03ef609bd85e98f1ce6da7fa042ef00
Peter N Jenkin <peternjenkin@gmail.com>
2019-05-04 18:35:02 +0100

    62 Simple notification sent to app (NB channel defined as well, extra to video)

--- app/src/main/java/com/example/notifications61/MainActivity.java
+++ app/src/main/java/com/example/notifications61/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.notifications61;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -34,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
         // New notification
         notification = new NotificationCompat.Builder(this);
+        // Notifications deprecated w/o a channel, but channel assigned in notificationButtonClicked
         notification.setAutoCancel(true);       // disappear automatically when clicked-on
-
     }
 
     @Override
@@ -43,6 +49,59 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    // Called on CLick to build notification & send
+    public void notificationButtonClicked(View view)
+    {
+
+        // Ready to build notification
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+
+        // Check to see whether OS/API d'demand a channel on notifications; if so, build same
+        // Define a notification channel https://stackoverflow.com/a/47974065/11365317
+        // https://developer.android.com/reference/android/app/NotificationChannel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            String CHANNEL_ID = "pnj_channel_01";
+            // NB CharSequence https://www.baeldung.com/java-char-sequence-string
+            CharSequence name = "pnj_channel";
+            String Description = "This is pnj's channel (for notifications &c)";
+            // capital initial-why? For string as immutable?
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[] {100, 200, 300, 400, 500, 400, 300, 200, 400});
+            // delay,vibrate, pause, vibrate, pause, ... https://proandroiddev.com/using-vibrate-in-android-b0e3ef5d5e07
+            mChannel.setShowBadge(false);
+            // badge on app icon enumerating any notifications received for this app
+
+            notificationManager.createNotificationChannel(mChannel);
+            this.notification.setChannelId(notificationManager.getNotificationChannel(CHANNEL_ID).getId());
+            // Ab initio, the notification had no channel TODO: maybe improve sequential logic of this
+        }
+
+        // Use default icon (for now)
+        notification.setSmallIcon(R.drawable.ic_launcher_foreground);
+        notification.setTicker("This is the PNJ ticker");       // to appear in the status bar
+        notification.setWhen(System.currentTimeMillis());       // appearing in rhs of notification
+        notification.setContentTitle("Here's the title of the notification");
+        notification.setContentText("Here's the body text of the notification");
+
+        // Send user back to home screen of app on click of notification (simple)
+        Intent i = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent
+                .getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+        // requestCode, intent, flags
+        // Give device access to form this intent's actvitity from intents on device
+        notification.setContentIntent(pendingIntent);
+
+        // Issue notification (issue: send to device using Notification Manager)
+        notificationManager.notify(uuid, notification.build());
     }
 
     @Override
